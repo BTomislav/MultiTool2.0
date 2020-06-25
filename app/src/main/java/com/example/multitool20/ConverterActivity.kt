4f2eb9a23259375108e119a14426beb0c52ec1eb
Tomislav <tomislav.besvir10@gmail.com>
2020-06-25 13:09:40 +0200

    Converter Animations

--- app/src/main/java/com/example/multitool20/ConverterActivity.kt
+++ app/src/main/java/com/example/multitool20/ConverterActivity.kt
@@ -8,7 +8,10 @@ import android.widget.Spinner
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_converter.*
 
-var swap=true
+lateinit var a: Spinner
+lateinit var b: Spinner
+lateinit var c: Spinner
+
 
 class ConverterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +20,29 @@ class ConverterActivity : AppCompatActivity() {
 
         val spinner: Spinner = findViewById(R.id.spinner)
         val spinner2: Spinner = findViewById(R.id.spinner2)
-        val spinner3: Spinner = findViewById(R.id.spinner3)
+        val spinnerCategory: Spinner = findViewById(R.id.spinner_category)
+        val buttonSwap: Button = findViewById(R.id.button3)
+        a=spinner
+        b=spinner2
+
+        buttonSwap.setOnClickListener {
+            if (motionLayout.progress==0.0f){
+                motionLayout.transitionToEnd()
+                spinner.setBackgroundResource(R.drawable.custom_spinner2)
+                spinner2.setBackgroundResource(R.drawable.custom_spinner)
+                SwapSpinners()
+                //Toast.makeText(this, ""+a.selectedItem.toString()+" je prvi, a "+b.selectedItem.toString()+" je drugi", Toast.LENGTH_SHORT).show()
+            }
+
+            if (motionLayout.progress==1.0f){
+                motionLayout.transitionToStart()
+                spinner.setBackgroundResource(R.drawable.custom_spinner)
+                spinner2.setBackgroundResource(R.drawable.custom_spinner2)
+                SwapSpinners()
+                //Toast.makeText(this, ""+a.selectedItem.toString()+" je prvi, a "+b.selectedItem.toString()+" je drugi", Toast.LENGTH_SHORT).show()
+            }
+
+        }
 
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter.createFromResource(
@@ -50,7 +75,13 @@ class ConverterActivity : AppCompatActivity() {
             // Specify the layout to use when the list of choices appears
             adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             // Apply the adapter to the spinner
-            spinner3.adapter = adapter3
+            spinnerCategory.adapter = adapter3
         }
+    }
+
+    private fun SwapSpinners() {
+        c= a
+        a= b
+        b=c
     }
 }
