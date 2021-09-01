0fc90efe392b5d9c73d4787820ca54a8c63ca53d
Tomislav <tomislav.besvir10@gmail.com>
2021-09-01 13:44:30 +0200

    Converter Animations

--- app/src/main/java/com/example/multitool20/ConverterActivity.kt
+++ app/src/main/java/com/example/multitool20/ConverterActivity.kt
@@ -2,28 +2,35 @@ package com.example.multitool20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_converter.*
 
 lateinit var a: Spinner
 lateinit var b: Spinner
 lateinit var c: Spinner
 
-
 class ConverterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_converter)
 
-        val spinner: Spinner = findViewById(R.id.spinner)
-        val spinner2: Spinner = findViewById(R.id.spinner2)
-        val spinnerCategory: Spinner = findViewById(R.id.spinner_category)
+        val spinner= findViewById<Spinner>(R.id.spinner)
+        val spinner2 = findViewById<Spinner>(R.id.spinner2)
+        val spinnerCategory= findViewById<Spinner>(R.id.spinner_category)
         val buttonSwap: Button = findViewById(R.id.button3)
+
         a=spinner
         b=spinner2
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.category,
+            android.R.layout.simple_spinner_item
+        ).also { adapter3 ->
+            adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerCategory.adapter = adapter3
+        }
 
         buttonSwap.setOnClickListener {
             if (motionLayout.progress==0.0f){
@@ -31,7 +38,6 @@ class ConverterActivity : AppCompatActivity() {
                 spinner.setBackgroundResource(R.drawable.custom_spinner2)
                 spinner2.setBackgroundResource(R.drawable.custom_spinner)
                 SwapSpinners()
-                //Toast.makeText(this, ""+a.selectedItem.toString()+" je prvi, a "+b.selectedItem.toString()+" je drugi", Toast.LENGTH_SHORT).show()
             }
 
             if (motionLayout.progress==1.0f){
@@ -39,44 +45,89 @@ class ConverterActivity : AppCompatActivity() {
                 spinner.setBackgroundResource(R.drawable.custom_spinner)
                 spinner2.setBackgroundResource(R.drawable.custom_spinner2)
                 SwapSpinners()
-                //Toast.makeText(this, ""+a.selectedItem.toString()+" je prvi, a "+b.selectedItem.toString()+" je drugi", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        spinnerCategory.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
             }
 
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                when(position){
+                    0 -> {
+                        ArrayAdapter.createFromResource(
+                        this@ConverterActivity,
+                        R.array.temperature,
+                        android.R.layout.simple_spinner_item
+                    ).also { spinnerAdapter ->
+                        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                        spinner.adapter = spinnerAdapter
+                        spinner2.adapter= spinnerAdapter
+                    }
+                    }
+                    1 -> {
+                        ArrayAdapter.createFromResource(
+                            this@ConverterActivity,
+                            R.array.length,
+                            android.R.layout.simple_spinner_item
+                        ).also { spinnerAdapter ->
+                            spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                            spinner.adapter = spinnerAdapter
+                            spinner2.adapter= spinnerAdapter
+                        }
+                    }
+                    2 -> {
+                        ArrayAdapter.createFromResource(
+                            this@ConverterActivity,
+                            R.array.currency,
+                            android.R.layout.simple_spinner_item
+                        ).also { spinnerAdapter ->
+                            spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                            spinner.adapter = spinnerAdapter
+                            spinner2.adapter= spinnerAdapter
+                        }
+                    }
+                    3 -> {
+                        ArrayAdapter.createFromResource(
+                            this@ConverterActivity,
+                            R.array.weight,
+                            android.R.layout.simple_spinner_item
+                        ).also { spinnerAdapter ->
+                            spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                            spinner.adapter = spinnerAdapter
+                            spinner2.adapter= spinnerAdapter
+                        }
+                    }
+                    4 -> {
+                        ArrayAdapter.createFromResource(
+                            this@ConverterActivity,
+                            R.array.speed,
+                            android.R.layout.simple_spinner_item
+                        ).also { spinnerAdapter ->
+                            spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                            spinner.adapter = spinnerAdapter
+                            spinner2.adapter= spinnerAdapter
+                        }
+                    }
+                    5-> {
+                        ArrayAdapter.createFromResource(
+                            this@ConverterActivity,
+                            R.array.time,
+                            android.R.layout.simple_spinner_item
+                        ).also { spinnerAdapter ->
+                            spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                            spinner.adapter = spinnerAdapter
+                            spinner2.adapter= spinnerAdapter
+                        }
+                    }
+                }
+            }
         }
 
-// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.planets_array,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            spinner.adapter = adapter
-        }
 
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.planets_array,
-            android.R.layout.simple_spinner_item
-        ).also { adapter2 ->
-            // Specify the layout to use when the list of choices appears
-            adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            spinner2.adapter = adapter2
-        }
 
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.category,
-            android.R.layout.simple_spinner_item
-        ).also { adapter3 ->
-            // Specify the layout to use when the list of choices appears
-            adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            spinnerCategory.adapter = adapter3
-        }
+
     }
 
     private fun SwapSpinners() {
